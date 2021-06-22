34e1e8099cc56695c9268ddff48fdb3c7c8a305f
Elauren77 <elaurent@mad-77222L>
2021-06-22 14:17:23 -0400

    try it again

--- dotnetTest/Program.cs
+++ dotnetTest/Program.cs
@@ -6,7 +6,7 @@ namespace dotnetTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");    
+            Console.WriteLine("Hello World!");        
         }
     }
 }
